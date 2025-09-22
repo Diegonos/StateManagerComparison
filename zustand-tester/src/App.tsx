9a0d0e4bc67f01837c669054a68ff4fc9e295069
Diego Nosari <diego_nos@hotmail.it>
2025-09-22 08:58:18 +0200

    Added comment

--- zustand-tester/src/App.tsx
+++ zustand-tester/src/App.tsx
@@ -3,6 +3,12 @@ import Counter from './components/Counter';
 import { useCounterStore } from './store/store';
 
 function App() {
+  /* 
+    obtaining the count like this:
+    const { count } = useCounterStore((state) => state);
+    is against the best practices and could slow down your app a lot.
+    You should be doing as below.
+  */
   const count = useCounterStore((state) => state.count);
 
   return <Counter count={count} />;
